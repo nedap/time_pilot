55ec5261cafd0fd88efe57e5d18a69e8cd96661e
Matthijs Langenberg <matthijs.langenberg@nedap.com>
2015-07-27 14:49:59 +0200

    Code style

--- lib/time_pilot/configuration.rb
+++ lib/time_pilot/configuration.rb
@@ -1,5 +1,4 @@
 module TimePilot
-
   class Configuration
     attr_reader :features, :redis_store
 
@@ -8,13 +7,12 @@ module TimePilot
       @redis_store = Redis.new
     end
 
-    def feature feature_name
+    def feature(feature_name)
       @features.push(feature_name)
     end
 
-    def redis redis_store
+    def redis(redis_store)
       @redis_store = redis_store
     end
   end
-
 end
--- lib/time_pilot/time_pilot.rb
+++ lib/time_pilot/time_pilot.rb
@@ -9,26 +9,28 @@ module TimePilot
     end
   end
 
-  def self.group_classes
-    @group_classes
+  class << self
+    attr_reader :group_classes
   end
 
   def self.configure
     @config ||= Configuration.new
     yield @config
-    @config.features.each do |feature_name|
-      Features.module_eval do
-        define_method "enable_#{feature_name}" do
-          pilot_enable_feature(feature_name)
-        end
+    @config.features.each { |f| define_feature_method(f) }
+  end
 
-        define_method "disable_#{feature_name}" do
-          pilot_disable_feature(feature_name)
-        end
+  def self.define_feature_method(feature_name)
+    Features.module_eval do
+      define_method "enable_#{feature_name}" do
+        pilot_enable_feature(feature_name)
+      end
 
-        define_method "#{feature_name}_enabled?" do
-          pilot_feature_enabled?(feature_name)
-        end
+      define_method "disable_#{feature_name}" do
+        pilot_disable_feature(feature_name)
+      end
+
+      define_method "#{feature_name}_enabled?" do
+        pilot_feature_enabled?(feature_name)
       end
     end
   end
@@ -45,6 +47,7 @@ module TimePilot
     "#{NAMESPACE}:#{name}"
   end
 
+  # Including this module makes a class a TimePilot class
   module Features
     def self.included(base)
       base.send :extend, ClassMethods
@@ -52,31 +55,35 @@ module TimePilot
 
     module ClassMethods
       attr_reader :time_pilot_groups
-      def is_pilot_group options={}
+      def is_pilot_group(options = {})
         TimePilot.register_class(self)
-        @time_pilot_groups = Array(options[:overridden_by]).map { |e| e.to_s } + [self.to_s.underscore]
+        @time_pilot_groups = Array(options[:overridden_by]).map(&:to_s) +
+                             [to_s.underscore]
       end
 
       def pilot_feature_cardinality(feature_name)
-        TimePilot.redis.scard TimePilot.key("#{feature_name}:#{self.to_s.underscore}_ids")
+        key_name = "#{feature_name}:#{to_s.underscore}_ids"
+        TimePilot.redis.scard TimePilot.key(key_name)
       end
     end
 
     def pilot_enable_feature(feature_name)
-      TimePilot.redis.sadd TimePilot.key("#{feature_name}:#{self.class.to_s.underscore}_ids"), id
+      key_name = "#{feature_name}:#{self.class.to_s.underscore}_ids"
+      TimePilot.redis.sadd TimePilot.key(key_name), id
     end
 
     def pilot_disable_feature(feature_name)
-      TimePilot.redis.srem TimePilot.key("#{feature_name}:#{self.class.to_s.underscore}_ids"), id
+      key_name = "#{feature_name}:#{self.class.to_s.underscore}_ids"
+      TimePilot.redis.srem TimePilot.key(key_name), id
     end
 
     def pilot_feature_enabled?(feature_name)
-      TimePilot.redis.pipelined {
+      TimePilot.redis.pipelined do
         self.class.time_pilot_groups.each do |group|
           method = group.to_s == self.class.to_s.underscore ? 'id' : group + '_id'
           TimePilot.redis.sismember TimePilot.key("#{feature_name}:#{group}_ids"), send(method)
         end
-      }.include? true
+      end.include? true
     end
 
   end
