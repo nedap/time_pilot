628726137ad99a0db8352fbc122296898607bcaf
Abdelkader Boudih <terminale@gmail.com>
2021-09-15 12:38:01 +0100

    chore: switch to github actions

--- test/time_pilot_test.rb
+++ test/time_pilot_test.rb
@@ -1,10 +1,13 @@
-require_relative '../lib/time_pilot'
-require 'minitest/autorun'
+# frozen_string_literal: true
+
+require_relative "../lib/time_pilot"
+require "minitest/autorun"
 
 class Company
   include TimePilot::Features
   is_pilot_group
   attr_reader :id
+
   def initialize(id)
     @id = id
   end
@@ -14,6 +17,7 @@ class Team
   include TimePilot::Features
   is_pilot_group overridden_by: :company
   attr_reader :company_id, :id
+
   def initialize(company_id, id)
     @company_id = company_id
     @id = id
@@ -22,8 +26,9 @@ end
 
 class Employee
   include TimePilot::Features
-  is_pilot_group overridden_by: [:company, :team]
+  is_pilot_group overridden_by: %i[company team]
   attr_reader :company_id, :team_id, :id
+
   def initialize(company_id, team_id, id)
     @company_id = company_id
     @team_id = team_id
@@ -32,11 +37,11 @@ class Employee
 end
 
 TimePilot.configure do |c|
-  c.feature 'planning'
+  c.feature "planning"
 end
 
 TimePilot.configure do |c|
-  c.feature 'secret_feature'
+  c.feature "secret_feature"
 end
 
 describe TimePilot do
@@ -50,102 +55,102 @@ describe TimePilot do
     @jane = Employee.new(@nedap.id, @healthcare.id, 22)
   end
 
-  it 'allows multiple configure blocks to add features' do
-    TimePilot.features.must_equal ['planning', 'secret_feature']
-    @acme.planning_enabled?.must_equal false
-    @acme.secret_feature_enabled?.must_equal false
+  it "allows multiple configure blocks to add features" do
+    _(TimePilot.features).must_equal %w[planning secret_feature]
+    _(@acme.planning_enabled?).must_equal false
+    _(@acme.secret_feature_enabled?).must_equal false
   end
 
-  it 'defines a getter on company' do
-    TimePilot.redis.sadd 'timepilot:planning:company_ids', @acme.id
-    @acme.planning_enabled?.must_equal true
-    @acme.instance_variable_get("@planning_enabled").must_equal true
+  it "defines a getter on company" do
+    TimePilot.redis.sadd "timepilot:planning:company_ids", @acme.id
+    _(@acme.planning_enabled?).must_equal true
+    _(@acme.instance_variable_get("@planning_enabled")).must_equal true
   end
 
-  it 'defines an enabler on company' do
+  it "defines an enabler on company" do
     @nedap.enable_planning
-    TimePilot.redis.sismember("timepilot:planning:company_ids", @nedap.id).must_equal true
+    _(TimePilot.redis.sismember("timepilot:planning:company_ids", @nedap.id)).must_equal true
   end
 
-  it 'defines a disabler on employee' do
+  it "defines a disabler on employee" do
     @nedap.enable_planning
-    @nedap.planning_enabled?.must_equal true
-    @nedap.instance_variable_get("@planning_enabled").must_equal true
+    _(@nedap.planning_enabled?).must_equal true
+    _(@nedap.instance_variable_get("@planning_enabled")).must_equal true
     @nedap.disable_planning
-    @nedap.planning_enabled?.must_equal false
-    @nedap.instance_variable_get("@planning_enabled").must_equal false
+    _(@nedap.planning_enabled?).must_equal false
+    _(@nedap.instance_variable_get("@planning_enabled")).must_equal false
   end
 
-  it 'defines a getter on team' do
-    TimePilot.redis.sadd 'timepilot:planning:team_ids', @healthcare.id
-    @healthcare.planning_enabled?.must_equal true
+  it "defines a getter on team" do
+    TimePilot.redis.sadd "timepilot:planning:team_ids", @healthcare.id
+    _(@healthcare.planning_enabled?).must_equal true
   end
 
-  it 'defines an enabler on team' do
+  it "defines an enabler on team" do
     @retail.enable_planning
-    TimePilot.redis.sismember("timepilot:planning:team_ids", @retail.id).must_equal true
+    _(TimePilot.redis.sismember("timepilot:planning:team_ids", @retail.id)).must_equal true
   end
 
-  it 'defines a disabler on team' do
+  it "defines a disabler on team" do
     @retail.enable_planning
-    @retail.planning_enabled?.must_equal true
-    @retail.instance_variable_get("@planning_enabled").must_equal true
+    _(@retail.planning_enabled?).must_equal true
+    _(@retail.instance_variable_get("@planning_enabled")).must_equal true
     @retail.disable_planning
-    @retail.planning_enabled?.must_equal false
-    @retail.instance_variable_get("@planning_enabled").must_equal false
+    _(@retail.planning_enabled?).must_equal false
+    _(@retail.instance_variable_get("@planning_enabled")).must_equal false
   end
 
-  it 'defines a getter on employee' do
-    TimePilot.redis.sadd 'timepilot:planning:employee_ids', @john.id
-    @john.planning_enabled?.must_equal true
-    @john.instance_variable_get("@planning_enabled").must_equal true
+  it "defines a getter on employee" do
+    TimePilot.redis.sadd "timepilot:planning:employee_ids", @john.id
+    _(@john.planning_enabled?).must_equal true
+    _(@john.instance_variable_get("@planning_enabled")).must_equal true
   end
 
-  it 'defines an enabler on employee' do
+  it "defines an enabler on employee" do
     @jane.enable_planning
-    TimePilot.redis.sismember("timepilot:planning:employee_ids", @jane.id).must_equal true
+    _(TimePilot.redis.sismember("timepilot:planning:employee_ids", @jane.id)).must_equal true
   end
 
-  it 'defines a disabler on employee' do
+  it "defines a disabler on employee" do
     @jane.enable_planning
-    @jane.planning_enabled?.must_equal true
-    @jane.instance_variable_get("@planning_enabled").must_equal true
+    _(@jane.planning_enabled?).must_equal true
+    _(@jane.instance_variable_get("@planning_enabled")).must_equal true
     @jane.disable_planning
-    @jane.planning_enabled?.must_equal false
-    @jane.instance_variable_get("@planning_enabled").must_equal false
+    _(@jane.planning_enabled?).must_equal false
+    _(@jane.instance_variable_get("@planning_enabled")).must_equal false
   end
 
-  it 'defines a cardinality count on the classes' do
+  it "defines a cardinality count on the classes" do
     @nedap.enable_planning
     @john.enable_planning
     @jane.enable_planning
-    Company.pilot_feature_cardinality(:planning).must_equal 1
-    Team.pilot_feature_cardinality(:planning).must_equal 0
-    Employee.pilot_feature_cardinality(:planning).must_equal 2
+    _(Company.pilot_feature_cardinality(:planning)).must_equal 1
+    _(Team.pilot_feature_cardinality(:planning)).must_equal 0
+    _(Employee.pilot_feature_cardinality(:planning)).must_equal 2
   end
 
-  it 'registers classes that include TimePilot::Features' do
-    TimePilot.group_classes.must_include Company
-    TimePilot.group_classes.must_include Team
-    TimePilot.group_classes.must_include Employee
+  it "registers classes that include TimePilot::Features" do
+    _(TimePilot.group_classes).must_include Company
+    _(TimePilot.group_classes).must_include Team
+    _(TimePilot.group_classes).must_include Employee
   end
 
-  specify 'company overrides team' do
+  specify "company overrides team" do
     @nedap.enable_planning
-    @retail.planning_enabled?.must_equal true
-    @retail.instance_variable_get("@planning_enabled").must_equal true
+    _(@retail.planning_enabled?).must_equal true
+    _(@retail.instance_variable_get("@planning_enabled")).must_equal true
   end
 
-  specify 'team overrides employee' do
+  specify "team overrides employee" do
     @healthcare.enable_planning
-    @john.planning_enabled?.must_equal true
-    @john.instance_variable_get("@planning_enabled").must_equal true
+    _(@john.planning_enabled?).must_equal true
+    _(@john.instance_variable_get("@planning_enabled")).must_equal true
   end
 
-  specify 'company overrides employee' do
+  specify "company overrides employee" do
     @nedap.enable_planning
-    @jane.planning_enabled?.must_equal true
-    @jane.instance_variable_get("@planning_enabled").must_equal true 
+    _(@jane.planning_enabled?).must_equal true
+    _(@jane.instance_variable_get("@planning_enabled")).must_equal true
   end
 end
 
@@ -154,9 +159,9 @@ class CamelCasedModel
   is_pilot_group
 end
 
-describe TimePilot, 'converting CamelCase to camel_case' do
+describe TimePilot, "converting CamelCase to camel_case" do
   it do
-    CamelCasedModel.time_pilot_groups.must_equal ['camel_cased_model']
+    _(CamelCasedModel.time_pilot_groups).must_equal ["camel_cased_model"]
   end
 end
 
@@ -176,34 +181,33 @@ describe TimePilot do
     end
   end
 
-  specify 'calls redis only once' do
+  specify "calls redis only once" do
     @mock.expect(:pipelined, [true])
 
-    @acme.planning_enabled?.must_equal true
+    _(@acme.planning_enabled?).must_equal true
     # Call the feature a second time
     # If it calls redis you will get
     # MockExpectationError: No more expects available for :pipelined: []
-    @acme.planning_enabled?.must_equal true
+    _(@acme.planning_enabled?).must_equal true
   end
 
-  specify 'does not call redis to get status after disabling' do
-    @mock.expect(:srem, true, ['timepilot:planning:company_ids', @acme.id])
+  specify "does not call redis to get status after disabling" do
+    @mock.expect(:srem, true, ["timepilot:planning:company_ids", @acme.id])
 
     @acme.disable_planning
     # Call the feature again it should hit the instance variable
     # If it calls redis you will get
     # MockExpectationError: No more expects available for :pipelined: []
-    @acme.planning_enabled?.must_equal false
+    _(@acme.planning_enabled?).must_equal false
   end
 
-  specify 'does not call redis to get status after enabling' do
-    @mock.expect(:sadd, true, ['timepilot:planning:company_ids', @acme.id])
+  specify "does not call redis to get status after enabling" do
+    @mock.expect(:sadd, true, ["timepilot:planning:company_ids", @acme.id])
 
     @acme.enable_planning
     # Call the feature again it should hit the instance variable
     # If it calls redis you will get
     # MockExpectationError: No more expects available for :pipelined: []
-    @acme.planning_enabled?.must_equal true
+    _(@acme.planning_enabled?).must_equal true
   end
 end
-
